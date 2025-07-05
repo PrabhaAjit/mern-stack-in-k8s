2ddb798b9413ead08aeb890d944c5ccae73245c3
PrabhaAjit <prabha.ajit@gmail.com>
2025-07-05 13:35:27 +0300

    record removed from .js file

--- server/routes/record.js
+++ server/routes/record.js
@@ -4,7 +4,7 @@ const dbo = require("../db/conn")
 const ObjectId = require("mongodb").ObjectId
 
 // This section will help you get a list of all the records.
-recordRoutes.route("/record").get(async function (req, res) {
+recordRoutes.route("/").get(async function (req, res) {
     const db_connect = dbo.getDb()
     try {
         const result = await db_connect.collection("records").find({}).toArray()
@@ -15,7 +15,7 @@ recordRoutes.route("/record").get(async function (req, res) {
 })
 
 // This section will help you get a single record by id
-recordRoutes.route("/record/:id").get(async function (req, res) {
+recordRoutes.route("/:id").get(async function (req, res) {
     const db_connect = dbo.getDb()
     const myquery = { _id: new ObjectId(req.params.id) }
     try {
@@ -27,7 +27,7 @@ recordRoutes.route("/record/:id").get(async function (req, res) {
 })
 
 // This section will help you create a new record.
-recordRoutes.route("/record/add").post(async function (req, res) {
+recordRoutes.route("/add").post(async function (req, res) {
     const db_connect = dbo.getDb()
     const myobj = {
         name: req.body.name,
