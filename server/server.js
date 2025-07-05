f8691e8f850d5093bd3eaa389542f3fa7e7e3c6d
PrabhaAjit <prabha.ajit@gmail.com>
2025-07-05 12:56:26 +0300

    server.js updated

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 
 //app.use(require("./routes/record"))
 const recordRouter = require("./routes/record")
-app.use("/api/record", recordRouter)
+app.use("/", recordRouter)
 
 const dbo = require("./db/conn")
 
